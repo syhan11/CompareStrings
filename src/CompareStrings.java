73fc564b54f5f7dae8bcce398883377a9453126b
syhan11 <sueyoung.6311@gmail.com>
2019-06-13 10:45:58 -0400

    2nd commit: using equals method

--- src/CompareStrings.java
+++ src/CompareStrings.java
@@ -7,11 +7,24 @@ public class CompareStrings {
         System.out.println("How is the weather? (rain, windy, snow, etc)");
         String answer = sc.nextLine();
 
+        /*** using compareTo method
         if (answer.compareTo("rain") == 0) {
             System.out.println("Take your umbrella!");
         } else if (answer.compareTo("windy") == 0) {
             System.out.println("Wear your jacket!");
         } else if (answer.compareTo("snow") == 0) {
+            System.out.println("Wear a coat and take a shovel!");
+        } else {
+            System.out.println("Enjoy your day!");
+        }
+         ******/
+
+        // using String's equals method
+        if (answer.equals("rain")) {
+            System.out.println("Take your umbrella!");
+        } else if (answer.equals("windy")) {
+            System.out.println("Wear your jacket!");
+        } else if (answer.equals("snow")) {
             System.out.println("Wear a coat and take a shovel!");
         } else {
             System.out.println("Enjoy your day!");
